60a36fc7700a53ef91ae1a7fcc884c708ae44614
Alec Keeler <alec.keeler@gmail.com>
2022-02-22 11:28:41 -0600

    w10d2 task 3

--- sql_stuff/sql_code.sql
+++ sql_stuff/sql_code.sql
@@ -33,3 +33,32 @@ CREATE TABLE reviews (
 );
 
 ALTER TABLE players ADD COLUMN prefers_video_games BOOLEAN DEFAULT false;
+
+
+-- Task 3
+INSERT INTO boardgames (name, avg_rating, max_players, category)
+VALUES
+    ('Gloomhaven', 8.8, 4, 'Adventure'),
+    ('Pandemic Legacy: Season 1', 8.62, 4, 'Cooperative'),
+    ('Brass: Birmingham', 8.66, 4, 'Economic'),
+    ('Terraforming Mars', 8.43, 5, 'Economic'),
+    ('Twilight Imperium: Fourth Edition', 8.7, 6, 'Strategy'),
+    ('Spirit Island', 8.34, 4, 'Cooperative'),
+    ('Mage Knight', 8.1, 4, 'Adventure'),
+    ('Rising Sun', 7.88, 5, 'Strategy');
+
+INSERT INTO players (name, fave_category, prefers_video_games)
+VALUES
+    ('Alec', 'Strategy', DEFAULT),
+    ('Brad', 'Cooperative', DEFAULT),
+    ('Rawaha', 'Adventure', DEFAULT),
+    ('Dan', 'Economic', true),
+    ('Drew', 'Strategy', DEFAULT),
+    ('Ray', 'Economic', DEFAULT);
+
+INSERT INTO reviews (content, boardgame_id)
+VALUES
+    ('There is nothing I love more than escaping one pandemic for another, 10/10', 2),
+    ('This game is far too long!', 5),
+    ('My friends and I really like this game, lots of replayability', 6),
+    ('I can be a space pirate, favorite game hands down.', 5);

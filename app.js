1ff587758f59767a88256602a878f891237324ab
Alec Keeler <alec.keeler@gmail.com>
2022-03-01 10:57:36 -0600

    w11d2t18

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const express = require('express');
 // Task 17a
 const app = express()
 
+// Task 18
+app.get('/', (req, res) => {
+    // console.log(req)
+    res.send('Hello from Breaddit')
+})
 
-
-// 17b
+// Task 17b
 const port = 8080;
 app.listen(port, () => console.log(`Listening on port ${port}`))

a9667347ee47cfab9a63cb08277bbb891d935de6
Alec Keeler <alec.keeler@gmail.com>
2022-03-01 11:58:32 -0600

    w11d2t20

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express');
-
+const { User } = require('./models');
 // Task 17a
-const app = express()
+const app = express();
+
+// Task 19a
+app.set('view engine', 'pug');
 
 // Task 18
-app.get('/', (req, res) => {
+// Task 20a
+app.get('/users', async(req, res) => {
     // console.log(req)
-    res.send('Hello from Breaddit')
+    // res.send('Hello from Breaddit')
+    // Task 20a
+    const users = await User.findAll()
+    // Task 19c
+    res.render('index', {header: 'Welcome to Breaddit!', users})
+})
+
+// Task 20c
+// GET /users/1
+app.get('/users/:id', async(req, res) => {
+    console.log(req.params)
+    const user = await User.findByPk(req.params.id)
+    res.render('index', {header: 'Profile Page', users: [user]})
 })
 
 // Task 17b
